4b749b53f18e0bbcc7f329dcf500de2e8f514ad9
Hannibal85090 <aldid220@gmail.com>
2026-03-18 23:03:55 +0100

    Update motion_engine.py

--- core/motion_engine.py
+++ core/motion_engine.py
@@ -1,20 +1,12 @@
 import cv2
 import numpy as np
-
-# محاولة استيراد Mediapipe بمرونة عالية
-try:
-    import mediapipe as mp
-    from mediapipe.python.solutions import hands as mp_hands
-    from mediapipe.python.solutions import drawing_utils as mp_draw
-except ImportError:
-    import mediapipe as mp
-    mp_hands = mp.solutions.hands
-    mp_draw = mp.solutions.drawing_utils
+import mediapipe as mp
 
 class FlashDealMotionEngine:
     def __init__(self):
-        self.mp_hands = mp_hands
-        self.mp_draw = mp_draw
+        # استخدام الاستدعاء المباشر من mp.solutions لتجنب AttributeError
+        self.mp_hands = mp.solutions.hands
+        self.mp_draw = mp.solutions.drawing_utils
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
             max_num_hands=1,
@@ -26,7 +18,7 @@ class FlashDealMotionEngine:
         if frame is None:
             return False, None
 
-        # تحويل الألوان من BGR إلى RGB
+        # تحويل الصورة إلى RGB
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = self.hands.process(rgb_frame)
         
@@ -37,9 +29,7 @@ class FlashDealMotionEngine:
                 self.mp_draw.draw_landmarks(
                     frame, 
                     hand_landmarks, 
-                    self.mp_hands.HAND_CONNECTIONS,
-                    self.mp_draw.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
-                    self.mp_draw.DrawingSpec(color=(0, 0, 255), thickness=2)
+                    self.mp_hands.HAND_CONNECTIONS
                 )
         
         return motion_detected, frame
