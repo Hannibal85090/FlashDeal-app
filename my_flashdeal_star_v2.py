9d4353d755a0c9e3424e3a088c8a5b4f2652d326
Hannibal85090 <aldid220@gmail.com>
2026-03-23 08:53:56 +0100

    Update my_flashdeal_star_v2.py

--- my_flashdeal_star_v2.py
+++ my_flashdeal_star_v2.py
@@ -70,7 +70,7 @@ with st.sidebar:
 
 # --- العنوان الجديد مع التاريخ والتوقيت ---
 current_time=time.strftime("%d/%m/%Y - %H:%M:%S")
-st.markdown("<h1 style='text-align:center;color:#000000;text-shadow:0 0 10px #444444;'>★ My FlashDeal Star ★</h1>",unsafe_allow_html=True)
+st.markdown("<h1 style='text-align:center;color:#000000;text-shadow:0 0 10px #444444;'>🌟 My FlashDeal Star 🌟</h1>",unsafe_allow_html=True)
 st.markdown(f"<p style='text-align:center;color:#4facfe;'>🕒 Current Time: {current_time}</p>",unsafe_allow_html=True)
 st.markdown('<div class="star">★</div>',unsafe_allow_html=True)
 
